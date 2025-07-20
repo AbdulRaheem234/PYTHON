a827e693c7a421565297afb84b7287fd248efee3
Abdul Raheem <149872465+AbdulRaheem234@users.noreply.github.com>
2025-07-20 18:25:40 +0530

    First Commit

---
+++ CHAPTER-8/03_functions_with_arguments.py
@@ -0,0 +1,13 @@
+def goodDay(name, ending):
+    print("Good Day, " + name)
+    print(ending)
+    # return "done"
+    return "OK"
+
+# goodDay("Harry", "Thank you")
+# goodDay("Rohan", "Thank you")
+# goodDay("Divya", "Thank you")
+# goodDay("Komal", "Thank you")
+
+a = goodDay("Abdul", "Thank you")
+print(a)
